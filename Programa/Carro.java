8b06b160e0db2d0b38a2cae57ced37ef62df8d5a
AngieUwU <>
2021-04-14 16:59:49 +0000

    Java

---
+++ Programa/Carro.java
@@ -0,0 +1,13 @@
+public class Carro{
+
+private static String color="Azul";
+private static String marca="Renault";
+private static String modelo="2020";
+
+  public static void main (String[] args){
+    System.out.println ("Este es un carro de: "); 
+    System.out.println ("Color: "+color); 
+    System.out.println ("Marca: "+marca); 
+    System.out.println ("Modelo: "+modelo); 
+  }
+}
